aff939366b95f9a1832b98141758986acf12659a
unknown <rapelangmaputsoe@gmail.com>
2024-12-04 11:13:57 +0200

    Remove files from the repository

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { registerUser } = require('../controllers/userController');
-
-const router = express.Router();
-
-router.post('/users', registerUser);
-
-module.exports = router;
